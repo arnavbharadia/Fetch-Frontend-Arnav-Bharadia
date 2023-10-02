aada2affd7ec8461e1081bd416feb7f48d477a14
Arnav Bharadia <abharadia@wisc.edu>
2023-10-01 23:52:48 -0500

    Commit (maybe last)
    a

--- app.js
+++ app.js
@@ -21,7 +21,81 @@ async function fetchBreeds() {
 
 // Fetch and display images for selected breeds
 async function loadImages() {
-    imageGallery.innerHTML = ""; // Clear previous images
+    const imageGrid = document.getElementById("image-grid");
+    imageGrid.innerHTML = ""; // Clear previous images
+
+    const selectedBreeds = Array.from(breedSelect.selectedOptions).map(option => option.value);
+
+    const numRows = 3; // Define the number of rows
+    const numCols = 4; // Define the number of columns
+
+    // Create rows and columns
+    for (let i = 0; i < numRows; i++) {
+        const row = document.createElement("div");
+        row.className = "row";
+
+        for (let j = 0; j < numCols; j++) {
+            const col = document.createElement("div");
+            col.className = "column";
+            row.appendChild(col);
+        }
+
+        imageGrid.appendChild(row);
+    }
+
+    const columns = document.querySelectorAll(".column");
+
+    let colIndex = 0;
+
+    for (const breed of selectedBreeds) {
+        try {
+            const response = await fetch(`https://dog.ceo/api/breed/${breed}/images/random/5`);
+            const data = await response.json();
+            const images = data.message;
+
+            images.forEach(imageUrl => {
+                const imgContainer = document.createElement("div");
+                imgContainer.className = "img-container";
+                const img = document.createElement("img");
+                img.src = imageUrl;
+                img.alt = breed;
+
+                columns[colIndex].appendChild(imgContainer);
+                imgContainer.appendChild(img);
+
+                // Add a hover effect to enlarge the image
+                imgContainer.addEventListener("mouseover", () => {
+                    img.style.transform = "scale(1.1)";
+                    img.style.transition = "transform 0.2s ease-in-out";
+                });
+
+                imgContainer.addEventListener("mouseout", () => {
+                    img.style.transform = "scale(1)";
+                });
+
+                colIndex = (colIndex + 1) % columns.length; // Cycle through columns
+            });
+        } catch (error) {
+            console.error(`Error fetching images for ${breed}:`, error);
+        }
+    }
+}
+
+// Call the loadImages function when the page loads
+window.addEventListener("load", loadImages);
+
+
+// Event listeners
+window.addEventListener("load", fetchBreeds);
+loadImagesButton.addEventListener("click", loadImages);
+
+// ... Your existing JavaScript ...
+
+// Fetch and display images for selected breeds
+async function loadImages() {
+    const imageGrid = document.getElementById("image-grid");
+    imageGrid.innerHTML = ""; // Clear previous images
+
     const selectedBreeds = Array.from(breedSelect.selectedOptions).map(option => option.value);
 
     for (const breed of selectedBreeds) {
@@ -36,8 +110,9 @@ async function loadImages() {
                 const img = document.createElement("img");
                 img.src = imageUrl;
                 img.alt = breed;
+
                 imgContainer.appendChild(img);
-                imageGallery.appendChild(imgContainer);
+                imageGrid.appendChild(imgContainer);
             });
         } catch (error) {
             console.error(`Error fetching images for ${breed}:`, error);
@@ -45,6 +120,39 @@ async function loadImages() {
     }
 }
 
-// Event listeners
-window.addEventListener("load", fetchBreeds);
-loadImagesButton.addEventListener("click", loadImages);
+// Call the loadImages function when the page loads
+window.addEventListener("load", loadImages);
+
+// Fetch and display images for selected breeds
+async function loadImages() {
+    const imageGrid = document.getElementById("image-grid");
+    imageGrid.innerHTML = ""; // Clear previous images
+
+    const selectedBreeds = Array.from(breedSelect.selectedOptions).map(option => option.value);
+
+    for (const breed of selectedBreeds) {
+        try {
+            const response = await fetch(`https://dog.ceo/api/breed/${breed}/images/random/5`);
+            const data = await response.json();
+            const images = data.message;
+
+            images.forEach(imageUrl => {
+                const imgContainer = document.createElement("div");
+                imgContainer.className = "img-container";
+                const img = document.createElement("img");
+                img.src = imageUrl;
+                img.alt = breed;
+
+                imgContainer.appendChild(img);
+                imageGrid.appendChild(imgContainer);
+            });
+        } catch (error) {
+            console.error(`Error fetching images for ${breed}:`, error);
+        }
+    }
+}
+
+// Call the loadImages function when the page loads
+window.addEventListener("load", loadImages);
+
+// ... Your existing event listeners ...
